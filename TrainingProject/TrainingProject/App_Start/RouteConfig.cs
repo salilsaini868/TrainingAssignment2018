50c987309ace04b0084e38c46d2640d612a6be8a
niharika-n <niharika.niharika@evry.com>
2018-07-27 17:37:35 +0530

    Login_New : Created new login page.

--- TrainingProject/TrainingProject/App_Start/RouteConfig.cs
+++ TrainingProject/TrainingProject/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TrainingProject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "home", action = "index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "LoginScreen", id = UrlParameter.Optional }
             );
         }
     }
