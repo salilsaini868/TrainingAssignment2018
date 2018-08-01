93803528113440d5c532c69a35a5ef551e1edac6
niharika-n <niharika.niharika@evry.com>
2018-08-01 16:31:02 +0530

    Delete_Product_Category : delete option created.

--- TrainingProject/TrainingProject/Scripts/CategoryList.js
+++ TrainingProject/TrainingProject/Scripts/CategoryList.js
@@ -1,9 +1,4 @@
-﻿$(document).ready(function () {
-    $('.link_edit').click(function (e) {
-        if (!window.confirm("Confirm Edit")) {
-            e.preventDefault();
-        }
-    });
+﻿$(document).ready(function () {    
     $('.link_delete').click(function (e) {
         if (!window.confirm("Confirm Delete")) {
             e.preventDefault();
