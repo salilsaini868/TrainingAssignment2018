b5b085ed2374fba06cefc81afcd5bca7f082c604
shivanshu2394 <shivanshu.sharma@evry.com>
2018-08-01 19:21:11 +0530

    changes done

--- TrainingProject/TrainingProject/Controllers/ProductController.cs
+++ TrainingProject/TrainingProject/Controllers/ProductController.cs
@@ -157,6 +157,7 @@ namespace TrainingProject.Controllers
         public ActionResult Listing(FormCollection collection)
         {
             string searchName = collection["name"];
+
             List<ProductModel> p_list = new List<ProductModel>();
             using (SqlConnection connect_search = new SqlConnection(strconnect))
             {
@@ -169,7 +170,19 @@ namespace TrainingProject.Controllers
                     ViewBag.search = searchName;
                     SqlCommand cmd_search = new SqlCommand("Select * from Training_Products where  Prod_Name Like '%' + @Prod_Name + '%' OR Product_Description Like '%' + @Prod_Name + '%'  ", connect_search);
                     cmd_search.Parameters.AddWithValue("@Prod_Name", searchName);
+                    SearchFunction(cmd_search);
 
+                }
+                else
+                {
+                    SqlCommand cmd_search = new SqlCommand("select * from Training_Products order by Product_ID DESC", connect_search);
+                    SearchFunction(cmd_search);
+
+                }
+
+                List<ProductModel> SearchFunction(SqlCommand cmd_search)
+                {
+                    
                     SqlDataReader reader = cmd_search.ExecuteReader();
                     while (reader.Read())
                     {
@@ -184,38 +197,24 @@ namespace TrainingProject.Controllers
                             IsActive = Convert.ToBoolean(reader["IsActive"])
                         };
                         p_list.Add(prop);
-                        //return View("ProductListing", p_list);
                     }
-                }
-                else
-                {
-                    SqlCommand cmd_search = new SqlCommand("select * from Training_Products order by Product_ID DESC", connect_search);
-                    SqlDataReader reader = cmd_search.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        ProductModel prop = new ProductModel
-                        {
-                            //Product_ID = Convert.ToInt32(reader["Product_ID"]),
-                            Product_name = Convert.ToString(reader["Prod_Name"]),
-                            Price = Convert.ToInt32(reader["Price"]),
-                            NoOfProducts = Convert.ToInt32(reader["No_Of_Products"]),
-                            Date = Convert.ToDateTime(reader["Visible_Till"]),
-                            Description = Convert.ToString(reader["Product_Description"]),
-                            IsActive = Convert.ToBoolean(reader["IsActive"])
-                        };
-                        p_list.Add(prop);
-
-                    }
+                    return p_list;
 
                 }
+
+
                 return View("ProductListing", p_list);
             }
-
-
+           
         }
-
+        
     }
+
 }
+
+
+
+
 
 
 
