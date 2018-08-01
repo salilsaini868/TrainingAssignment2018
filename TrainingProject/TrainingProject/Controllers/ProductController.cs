9cc70c7d0387a18a61609addf3e0c11c049854b2
shivanshu2394 <shivanshu.sharma@evry.com>
2018-08-01 15:38:19 +0530

    Search Operation

--- TrainingProject/TrainingProject/Controllers/ProductController.cs
+++ TrainingProject/TrainingProject/Controllers/ProductController.cs
@@ -9,7 +9,6 @@ namespace TrainingProject.Controllers
 {
     public class ProductController : Controller
     {
-
         string strconnect = string.Empty;
 
         public ProductController()
@@ -51,7 +50,7 @@ namespace TrainingProject.Controllers
                 }
                 connect.Close();
             }
-            return View("ProductListing",prod_list);
+            return View("ProductListing", prod_list);
         }
 
         [HttpGet]
@@ -84,7 +83,7 @@ namespace TrainingProject.Controllers
                     command.Parameters.AddWithValue("@Visible_Till", prop.Date);
                     command.Parameters.AddWithValue("@Product_Description", prop.Description);
                     command.Parameters.AddWithValue("@IsActive ", prop.IsActive);
-                    
+
                     int sucess = command.ExecuteNonQuery();
                     if (sucess > 0)
                     {
@@ -94,8 +93,61 @@ namespace TrainingProject.Controllers
                 }
             }
             return RedirectToAction("InsertProduct");
-            
+
+        }
+
+        [HttpPost]
+        public ActionResult Listing(FormCollection collection)
+        {
+            string searchName = collection["name"];
+            List<ProductModel> p_list = new List<ProductModel>();
+
+            if (!string.IsNullOrEmpty(searchName))
+            {
+                ViewBag.search = searchName;
+
+                using (SqlConnection connect_search = new SqlConnection(strconnect))
+                {
+                    if (connect_search.State != ConnectionState.Open)
+                    {
+                        connect_search.Open();
+                    }
+
+                    SqlCommand cmd_search = new SqlCommand("Select * from Training_Products where  Prod_Name Like '%' + @Prod_Name + '%' OR Product_Description Like '%' + @Prod_Name + '%'  ", connect_search);
+                    cmd_search.Parameters.AddWithValue("@Prod_Name", searchName);
+
+                    SqlDataReader reader = cmd_search.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        ProductModel prop = new ProductModel
+                        {
+                            Product_ID = Convert.ToInt32(reader["Product_ID"]),
+                            Product_name = Convert.ToString(reader["Prod_Name"]),
+                            Price = Convert.ToInt32(reader["Price"]),
+                            NoOfProducts = Convert.ToInt32(reader["No_Of_Products"]),
+                            Date = Convert.ToDateTime(reader["Visible_Till"]),
+                            Description = Convert.ToString(reader["Product_Description"]),
+                            IsActive = Convert.ToBoolean(reader["IsActive"])
+                        };
+                        p_list.Add(prop);
+                    }
+                }
+            }
+            else
+            {
+                ViewBag.SearchMessage = "Enter a Valid Input";
+               
+            }
+             return View("ProductListing", p_list);
         }
 
     }
 }
+
+
+//if (!string.IsNullOrEmpty(strSearch))
+//                {
+//                    ViewBag.searchQuery = strSearch; 
+//in controller
+
+//     <input name="txtSearch" type="text" autocomplete="off" id="txtSearch" value="@ViewBag.searchQuery"/>
