19061b54977138d6cc954e13e646be3d5fc021ba
shivanshu2394 <shivanshu.sharma@evry.com>
2018-08-14 18:22:47 +0530

    Bundling JavaScript and BootStrap.

--- TrainingProject/TrainingProject/App_Start/BundleConfig.cs
+++ TrainingProject/TrainingProject/App_Start/BundleConfig.cs
@@ -25,6 +25,35 @@ namespace TrainingProject
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            // create an object of ScriptBundle and 
+            // specify bundle name (as virtual path) as constructor parameter 
+            ScriptBundle javascriptBundle = new ScriptBundle("~/bundles/Project_Scripts");
+            ScriptBundle bootstrapBundle = new ScriptBundle("~/bundles/Bootstrap");
+            
+            //use Include() method to add all the script files with their paths 
+            javascriptBundle.Include(
+                           "~/Scripts/Product.js",
+                           "~/Scripts/Login.js",
+                           "~/Scripts/CategoryInsert.js",
+                           "~/Scripts/CategoryList.js",
+                           "~/Scripts/jquery - 1.10.2.min.js",
+                           "~/Scripts/jquery-3.2.1.js"
+                         );
+
+            bootstrapBundle.Include(
+                         "~/Scripts/bootstrap.min.js",
+                         "~/Scripts/modernizr-2.6.2.js",
+                         "~/Scripts/custom.js",
+                         "~/Scripts/popper.min.js"
+                );
+            
+            //Add the bundle into BundleCollection
+            bundles.Add(javascriptBundle);
+            bundles.Add(bootstrapBundle);
+            
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
