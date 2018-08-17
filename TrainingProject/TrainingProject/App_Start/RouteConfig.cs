19d6c140bd29118bcf2c68463a7a15b757b29bd8
shivanshu2394 <shivanshu.sharma@evry.com>
2018-08-17 16:39:40 +0530

    Attibute Routing on Dashboard, Product and Category.

--- TrainingProject/TrainingProject/App_Start/RouteConfig.cs
+++ TrainingProject/TrainingProject/App_Start/RouteConfig.cs
@@ -8,6 +8,26 @@ namespace TrainingProject
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                name: "Dashboard",
+                 url: "Dashboard",
+                defaults: new { controller = "Dashboard", action = "Index" }
+             );
+
+            routes.MapRoute(
+                name: "Product",
+                url: "Product",
+                defaults: new { controller = "Product", action = "Listing" }
+             );
+
+            routes.MapRoute(
+                 name: "Category",
+                 url: "Category",
+                 defaults: new { controller = "Category", action = "Listing" }
+             );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
