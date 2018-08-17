052061d4c50e32beafbf5e04b5a0b17742b6e9d1
shivanshu2394 <shivanshu.sharma@evry.com>
2018-08-17 15:50:09 +0530

    Created Common SQL Connection for Product Controller

--- TrainingProject/TrainingProject/Controllers/ProductController.cs
+++ TrainingProject/TrainingProject/Controllers/ProductController.cs
@@ -4,6 +4,8 @@ using System.Web.Mvc;
 using TrainingProject.Models;
 using System.Data;
 using System.Data.SqlClient;
+using TrainingProject.Helper;
+
 
 namespace TrainingProject.Controllers
 {
@@ -11,46 +13,28 @@ namespace TrainingProject.Controllers
     public class ProductController : Controller
     {
         string strconnect = string.Empty;
+        ConnectionHelper sqlconnect = new ConnectionHelper();
 
-        public ProductController()
-        {
-            strconnect = @"Data Source=172.20.21.129;MultipleActiveResultSets=True;Initial Catalog=RHPM;User ID=RHPM;Password=evry@123";
-        }
         public ActionResult Listing(FormCollection collection)
         {
-            SqlCommand cmd_search;
             string searchName = collection["txtSearch"];
             List<ProductModel> ListOfProducts = new List<ProductModel>();
-            using (SqlConnection connect_search = new SqlConnection(strconnect))
+            ViewBag.search = searchName;
+            List<KeyValuePair<string, object>> param = new List<KeyValuePair<string, object>>();
+            param.Add(new KeyValuePair<string, object>("SearchProduct", searchName));
+            var search_list = sqlconnect.CreateResult(executeType: ExecuteEnum.ListProduct, query: "Training_SearchProduct", command: CommandType.StoredProcedure, valuePairs: param);
+            ListOfProducts = SearchFunction(search_list);
+            var count = ListOfProducts.Count;
+            if (count == 0)
             {
-                if (connect_search.State != ConnectionState.Open)
-                {
-                    connect_search.Open();
-                }
-                ViewBag.search = searchName;
-
-                cmd_search = new SqlCommand("[dbo].[Training_SearchProduct]", connect_search);
-                cmd_search.CommandType = CommandType.StoredProcedure;
-                if (!string.IsNullOrEmpty(searchName))
-                {
-                    cmd_search.Parameters.AddWithValue("@searchProduct", searchName);
-                }
-
-                ListOfProducts = SearchFunction(cmd_search);
-
-                var count = ListOfProducts.Count;
-                if (count == 0)
-                {
-                    TempData["DataNotFound"] = "No records found.";
-                }
-
-                connect_search.Close();
-                return View("ProductListing", ListOfProducts);
+                TempData["DataNotFound"] = "No records found.";
             }
+            return View("ProductListing", ListOfProducts);
         }
         List<ProductModel> SearchFunction(SqlCommand cmd_search)
         {
             List<ProductModel> p_list = new List<ProductModel>();
+            List<KeyValuePair<string, object>> param = new List<KeyValuePair<string, object>>();
             SqlDataReader reader = cmd_search.ExecuteReader();
             while (reader.Read())
             {
@@ -70,7 +54,7 @@ namespace TrainingProject.Controllers
                     CreatedUser = Convert.ToString(reader["CreatedUser"]),
                     ModifiedUser = reader["ModifiedUser"] != DBNull.Value ? Convert.ToString(reader["ModifiedUser"]) : null,
                     ModifiedBy = reader["ModifiedBy"] != DBNull.Value ? Convert.ToInt32(reader["ModifiedBy"]) : 0,
-                    ModifiedDate = reader["ModifiedDate"] != DBNull.Value ? Convert.ToDateTime(reader["ModifiedDate"]) : default(DateTime)
+                    ModifiedDate = reader["ModifiedDate"] != DBNull.Value ? Convert.ToDateTime(reader["ModifiedDate"]) : default(DateTime),
                 };
                 p_list.Add(prop);
             }
@@ -81,145 +65,116 @@ namespace TrainingProject.Controllers
         public ActionResult InsertProduct()
         {
             GetCategories();
+            ViewBag.Message = "Insert Product";
+
             return View("ProductInsert");
         }
 
         [HttpPost]
         public ActionResult InsertUpdateProduct(ProductModel prop)
         {
-            SqlCommand command_InsertUpdate;
-            using (SqlConnection connect = new SqlConnection(strconnect))
+            var userlogin = Session["user"] as LoginModel;
+            List<KeyValuePair<string, object>> param = new List<KeyValuePair<string, object>>();
+            param.Add(new KeyValuePair<string, object>("Prod_Name", prop.Product_name));
+            param.Add(new KeyValuePair<string, object>("CategoryID", prop.CategoryID));
+            param.Add(new KeyValuePair<string, object>("Price", prop.Price));
+            param.Add(new KeyValuePair<string, object>("No_Of_Products", prop.NoOfProducts));
+            param.Add(new KeyValuePair<string, object>("Visible_Till", prop.VisibleDate));
+            param.Add(new KeyValuePair<string, object>("Product_Description", prop.Description));
+            param.Add(new KeyValuePair<string, object>("IsActive", prop.IsActive));
+
+
+            if (prop.Product_ID == 0)
             {
-                if (connect.State != ConnectionState.Open)
-                {
-                    connect.Open();
-                }
-                command_InsertUpdate = new SqlCommand("[dbo].[Training_Products_Insert]", connect);
-                command_InsertUpdate.CommandType = CommandType.StoredProcedure;
-                int InsertUpdate = InsertUpdateFunction(command_InsertUpdate, prop);
-                if (InsertUpdate > 0)
-                {
-                    TempData["DataInsertorUpdateMessage"] = prop.Product_ID > 0 ? "Data Updated" : "Data Inserted";
-                }
+                prop.CreatedUser = userlogin.Username;
+                param.Add(new KeyValuePair<string, object>("CreatedBy", userlogin.UserID));
+                param.Add(new KeyValuePair<string, object>("CreatedDate", DateTime.Now));
+
+                TempData["DataInsertorUpdateMessage"] = "Product Added.";
             }
+            else
+            {
+                param.Add(new KeyValuePair<string, object>("Product_ID", prop.Product_ID));
+                param.Add(new KeyValuePair<string, object>("ModifiedBy", userlogin.UserID));
+                param.Add(new KeyValuePair<string, object>("ModifiedDate", DateTime.Now));
+
+                TempData["DataInsertorUpdateMessage"] = "Product Updated.";
+            }
+            var command_insert = sqlconnect.CreateResult(executeType: ExecuteEnum.Insert, query: "Training_Products_Insert", command: CommandType.StoredProcedure, valuePairs: param);
+            int result = command_insert;
+
             return View("ProductInsert", prop);
         }
 
         public void GetCategories()
         {
-            using (SqlConnection con = new SqlConnection(strconnect))
+            List<KeyValuePair<string, object>> param = new List<KeyValuePair<string, object>>();
+
+            var command_insert = sqlconnect.CreateResult(executeType: ExecuteEnum.ListProduct, query: "Training_GetCategoryName", command: CommandType.StoredProcedure, valuePairs: param);
+            var result = command_insert;
+
+            SqlDataAdapter da = new SqlDataAdapter(result);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            List<SelectListItem> Categories = new List<SelectListItem>();
+            if (ds.Tables[0].Rows.Count > 0)
             {
-                if (con.State != ConnectionState.Open)
+                foreach (DataRow row in ds.Tables[0].Rows)
                 {
-                    con.Open();
-                }
-                SqlCommand cmd = new SqlCommand("Training_GetCategoryName", con)
-                {
-                    CommandType = CommandType.StoredProcedure
+                    Categories.Add(
+                        new SelectListItem
+                        {
+                            Value = Convert.ToString(row["CategoryID"]),
+                            Text = Convert.ToString(row["CategoryName"])
+                        });
                 };
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                DataSet ds = new DataSet();
-                da.Fill(ds);
-                List<SelectListItem> Categories = new List<SelectListItem>();
-                if (ds.Tables[0].Rows.Count > 0)
-                {
-                    foreach (DataRow row in ds.Tables[0].Rows)
-                    {
-                        Categories.Add(
-                            new SelectListItem
-                            {
-                                Value = Convert.ToString(row["CategoryID"]),
-                                Text = Convert.ToString(row["CategoryName"])
-                            });
-                    };
-                }
-                TempData["Categories"] = Categories;
-                TempData.Keep();
             }
-        }
-
-        int InsertUpdateFunction(SqlCommand command_InsertUpdate, ProductModel prop)
-        {
-            var userlogin = Session["user"] as LoginModel;
-            command_InsertUpdate.Parameters.AddWithValue("@Prod_Name", prop.Product_name);
-            command_InsertUpdate.Parameters.AddWithValue("@CategoryID", prop.CategoryID);
-            command_InsertUpdate.Parameters.AddWithValue("@Price", prop.Price);
-            command_InsertUpdate.Parameters.AddWithValue("@No_Of_Products", prop.NoOfProducts);
-            command_InsertUpdate.Parameters.AddWithValue("@Visible_Till", prop.VisibleDate);
-            command_InsertUpdate.Parameters.AddWithValue("@Product_Description", prop.Description);
-            command_InsertUpdate.Parameters.AddWithValue("@IsActive ", prop.IsActive);
-            if (prop.Product_ID == 0)
-            {
-                prop.CreatedUser = userlogin.Username;
-                command_InsertUpdate.Parameters.AddWithValue("@CreatedBy", userlogin.UserID);
-                command_InsertUpdate.Parameters.AddWithValue("@CreatedDate", DateTime.Now);
-            }
-            else
-            {
-                command_InsertUpdate.Parameters.AddWithValue("@Product_ID", prop.Product_ID);
-                command_InsertUpdate.Parameters.AddWithValue("@ModifiedBy", userlogin.UserID);
-                command_InsertUpdate.Parameters.AddWithValue("@ModifiedDate", DateTime.Now);
-            }
-            var SuccessMessage = command_InsertUpdate.ExecuteNonQuery();
-
-            return (SuccessMessage);
+            TempData["Categories"] = Categories;
+            TempData.Keep();
         }
 
         [HttpGet]
         public ActionResult GetProductByID(int? id)
         {
             GetCategories();
+            ViewBag.Message = "Update Product";
+
             ProductModel edit = new ProductModel();
 
-            using (SqlConnection connect_edit = new SqlConnection(strconnect))
+            if (id != 0)
             {
-                if (connect_edit.State != ConnectionState.Open)
-                {
-                    connect_edit.Open();
-                }
+                List<KeyValuePair<string, object>> param = new List<KeyValuePair<string, object>>();
+                param.Add(new KeyValuePair<string, object>("Product_ID", id));
+                var command_select = sqlconnect.CreateResult(executeType: ExecuteEnum.Detail, query: "Select * from Training_Products where Product_ID = @Product_ID", command: CommandType.Text, valuePairs: param);
 
-                if (id != 0)
+                command_select.Read();
                 {
-                    SqlCommand cmd_update = new SqlCommand("Select * from Training_Products where Product_ID = @Product_ID", connect_edit);
-                    cmd_update.Parameters.AddWithValue("@Product_ID", id);
-                    SqlDataReader reader = cmd_update.ExecuteReader();
-                    reader.Read();
-                    {
-                        edit.Product_ID = Convert.ToInt32(reader["Product_ID"]);
-                        edit.Product_name = Convert.ToString(reader["Prod_Name"]);
-                        edit.Price = Convert.ToInt32(reader["Price"]);
-                        edit.NoOfProducts = Convert.ToInt32(reader["No_Of_Products"]);
-                        edit.CategoryID = Convert.ToString(reader["CategoryID"]);
-                        edit.VisibleDate = Convert.ToDateTime(reader["Visible_Till"].ToString());
-                        edit.Description = Convert.ToString(reader["Product_Description"]);
-                        edit.IsActive = Convert.ToBoolean(reader["IsActive"]);
-                        edit.CreatedBy = Convert.ToInt32(reader["CreatedBy"]);
-                        edit.CreatedDate = Convert.ToDateTime(reader["CreatedDate"]);
-                        edit.ModifiedBy = reader["ModifiedBy"] != DBNull.Value ? Convert.ToInt32(reader["ModifiedBy"]) : 0;
-                        edit.ModifiedDate = reader["ModifiedDate"] != DBNull.Value ? Convert.ToDateTime(reader["ModifiedDate"]) : default(DateTime);
-                    }
+                    edit.Product_ID = Convert.ToInt32(command_select["Product_ID"]);
+                    edit.Product_name = Convert.ToString(command_select["Prod_Name"]);
+                    edit.Price = Convert.ToInt32(command_select["Price"]);
+                    edit.NoOfProducts = Convert.ToInt32(command_select["No_Of_Products"]);
+                    edit.CategoryID = Convert.ToString(command_select["CategoryID"]);
+                    edit.VisibleDate = Convert.ToDateTime(command_select["Visible_Till"].ToString());
+                    edit.Description = Convert.ToString(command_select["Product_Description"]);
+                    edit.IsActive = Convert.ToBoolean(command_select["IsActive"]);
+                    edit.CreatedBy = Convert.ToInt32(command_select["CreatedBy"]);
+                    edit.CreatedDate = Convert.ToDateTime(command_select["CreatedDate"]);
+                    edit.ModifiedBy = command_select["ModifiedBy"] != DBNull.Value ? Convert.ToInt32(command_select["ModifiedBy"]) : 0;
+                    edit.ModifiedDate = command_select["ModifiedDate"] != DBNull.Value ? Convert.ToDateTime(command_select["ModifiedDate"]) : default(DateTime);
                 }
-                return View("ProductInsert", edit);
             }
+            return View("ProductInsert", edit);
         }
-
         public ActionResult DeleteProduct(int ID)
         {
             DataTable dataset = new DataTable();
-            using (SqlConnection connect = new SqlConnection(strconnect))
-            {
-                if (connect.State != ConnectionState.Open)
-                {
-                    connect.Open();
-                }
-                SqlCommand DeleteCommand = new SqlCommand("[dbo].[Training_Products_Delete]", connect)
-                {
-                    CommandType = CommandType.StoredProcedure
-                };
-                DeleteCommand.Parameters.AddWithValue("@Product_ID", ID);
-                DeleteCommand.ExecuteNonQuery();
-                connect.Close();
-            }
+
+            List<KeyValuePair<string, object>> param = new List<KeyValuePair<string, object>>();
+            param.Add(new KeyValuePair<string, object>("Product_ID", ID));
+
+            var cmd_delete = sqlconnect.CreateResult(executeType: ExecuteEnum.Delete, query: "Training_Products_Delete", command: CommandType.StoredProcedure, valuePairs: param);
+            int del_user = cmd_delete;
+
             return RedirectToAction("Listing");
         }
     }
---
+++ TrainingProject/TrainingProject/Helper/ExecuteEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrainingProject.Helper
+{
+    public enum ExecuteEnum
+    {
+        Detail,
+        Insert,
+        Delete,
+        List,
+        ListProduct
+    }
+}
