18f3b21018c2bfaaa09b98d3ffea5fa78cecd2b2
niharika-n <niharika.niharika@evry.com>
2018-08-28 10:13:02 +0530

    Feature_add_Category_EntityFramework : category controller with entity framework.

--- TrainingProject/TrainingProject/Models/CategoryModel.cs
+++ TrainingProject/TrainingProject/Models/CategoryModel.cs
@@ -2,27 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TrainingProject.Models;
 
 namespace TrainingProject.Models
 {
-    public class CategoryModel
-    {
-        public int CategoryID
-        { get; set; }
-        public string CategoryName
-        { get; set; }
-        public string CategoryDescription
-        { get; set; }
-        public bool IsActive
-        { get; set; }
-        public int CreatedBy
-        { get; set; }
-        public DateTime CreatedDate
-        { get; set; }
-        public int ModifiedBy
-        { get; set; }
-        public DateTime ModifiedDate
-        { get; set; }
+    public class CategoryModel : Training_ProductCategories
+    {        
         public string ModifiedUser
         { get; set; }
         public string CreatedUser
