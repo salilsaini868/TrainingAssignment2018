dca52521efdbc142ec3893b6d045a5fffd2c2c87
niharika-n <niharika.niharika@evry.com>
2018-11-09 18:05:49 +0530

    Created DashboardController

---
+++ Backend/WebAPIs/WebAPIs/Controllers/DashboardController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIs.Data;
+using WebAPIs.Models;
+
+namespace WebAPIs.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly WebApisContext context;
+
+        public DashboardController(WebApisContext APIcontext)
+        {
+            context = APIcontext;
+        }        
+
+        [HttpGet]
+        public async Task<IActionResult> GetCount()
+        {
+            StatisticsModel statisticsModel = new StatisticsModel();
+
+            return Ok(statisticsModel);
+
+        }
+    }
+}
