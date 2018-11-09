4bec65f7ce3b90bfcf492a89f074228927be363d
niharika-n <niharika.niharika@evry.com>
2018-11-09 17:59:38 +0530

    Created StatisticsModel for dashboard

---
+++ Backend/WebAPIs/WebAPIs/Models/StatisticsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIs.Models
+{
+    public class StatisticsModel
+    {
+        public int CategoryCount
+        { get; set; }
+        public int ProductCount
+        { get; set; }
+    }
+}
