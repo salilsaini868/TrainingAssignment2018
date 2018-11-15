0d632900cff4f7a06c0c174d3a0e950c41820c29
niharika-n <niharika.niharika@evry.com>
2018-11-15 13:38:49 +0530

    Created CategoryModel.

---
+++ Backend/WebAPIs/WebAPIs/Models/CategoryModel.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIs.Models
+{
+    public class CategoryModel
+    {
+        [Key]
+        [BindNever]
+        public int CategoryID { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+        [Required]
+        public string CategoryDescription { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+        [Required]
+        [BindNever]
+        public int CreatedBy { get; set; }
+        [Required]
+        [BindNever]
+        public DateTime CreatedDate { get; set; }
+        [BindNever]
+        public Nullable<int> ModifiedBy { get; set; }
+        [BindNever]
+        public Nullable<DateTime> ModifiedDate { get; set; }
+    }    
+}
