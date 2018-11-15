1edfc03ee2b37ab7d3c4e7f5422fd45f2654a684
niharika-n <niharika.niharika@evry.com>
2018-11-15 13:40:51 +0530

    Created localCategoryModel

---
+++ Backend/WebAPIs/WebAPIs/Models/localModel/localCategoryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIs.Models.localModel
+{
+    public class localCategoryModel
+    {
+        public string ModifiedUser
+        { get; set; }
+        public string CreatedUser
+        { get; set; }
+    }
+}
